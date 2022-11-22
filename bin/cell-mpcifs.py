2be9e8d52a9a70c61f9728a8eab55a31ca4c082a
Se Hun Joo <shj29@cam.ac.uk>
2022-11-22 10:08:48 +0000

    update

---
+++ bin/cell-mpcifs.py
@@ -0,0 +1,53 @@
+from mp_api.client import MPRester
+
+api_key = "d7BCat3ve9lYksDLTojHg3AceBOcN5B9"
+mpr = MPRester(api_key)
+
+def mp_fields():
+    print("List of available fields")
+    list_of_available_fields = mpr.summary.available_fields
+    print(list_of_available_fields)
+
+def mp_example():
+    docs = mpr.summary.search(material_ids=["mp-1"])
+    print(docs[0])
+
+# get mpids
+
+def get_mpids_formula(formula):
+    docs = mpr.summary.search(formula=formula, fields=["material_id"])
+    return [doc.material_id for doc in docs]
+    
+def get_mpids_elements(elements):
+    docs = mpr.summary.search(elements=elements, fields=["material_id"])
+    return [doc.material_id for doc in docs]
+
+
+# to query summary data with Materials Project IDs
+
+def get_structures(mpids):
+
+    docs = mpr.summary.search(material_ids=mpids,
+                              fields=["material_id", "formula_pretty", "structure", "symmetry"])
+
+    for doc in docs:
+        mpid = doc.material_id
+        formula = doc.formula_pretty
+        sg = doc.symmetry.symbol
+        sg = sg.replace("/","").replace("_","")
+        structure = doc.structure
+
+        fmt = 'cif'
+        filename = formula + "_" + sg + "_"  + mpid + "." + fmt
+        print(filename)
+        #print(structure)
+        structure.to(filename = filename, fmt = fmt)
+
+
+#############################
+# Main
+#############################
+
+mp_fields()
+mp_example()
+get_structures(get_mpids_formula(["MoS2"]))
